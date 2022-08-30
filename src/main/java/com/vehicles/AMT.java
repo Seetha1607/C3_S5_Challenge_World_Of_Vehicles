cda4fa3d1cb69146baf10678cef18c6e212fa565
Seetha <seethalakshmi1607@gmail.com>
2022-08-31 00:04:58 +0530

    created a parametrized constructor and instantiated the values for different model number in AMT class

--- src/main/java/com/vehicles/AMT.java
+++ src/main/java/com/vehicles/AMT.java
@@ -21,4 +21,13 @@ public class AMT extends Transmission {
         this.thirdGearRatio = 1.510;
         this.fourthGearRatio = 1.000;
     }
+
+    public AMT(String modelNumber, int forwardGears, double firstGearRatio, double secondGearRatio, double thirdGearRatio, double fourthGearRatio, double fifthGearRatio) {
+        super("AMTD5", 5);
+        this.firstGearRatio = 2.950;
+        this.secondGearRatio = 1.940;
+        this.thirdGearRatio = 1.340;
+        this.fourthGearRatio = 1.000;
+        this.fifthGearRatio = 0.630;
+    }
 }
