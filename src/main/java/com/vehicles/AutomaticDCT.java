deec0044fbbb35744ec392681306ebb1869fbfa7
Seetha <seethalakshmi1607@gmail.com>
2022-08-31 00:48:21 +0530

    created parametrized constructor and instantiated the values in the constructor

--- src/main/java/com/vehicles/AutomaticDCT.java
+++ src/main/java/com/vehicles/AutomaticDCT.java
@@ -16,4 +16,16 @@ public class AutomaticDCT extends Transmission {
     private double sixthGearRatio;
     private double seventhGearRatio;
     private double eightGearRatio;
+
+    public AutomaticDCT(String transmissionType, String modelNumber, int forwardGears, double firstGearRatio, double secondGearRatio, double thirdGearRatio, double fourthGearRatio, double fifthGearRatio, double sixthGearRatio, double seventhGearRatio, double eightGearRatio) {
+        super("Automatic-DCT", "DCT8", 8);
+        this.firstGearRatio = 4.714;
+        this.secondGearRatio = 3.143;
+        this.thirdGearRatio = 2.106;
+        this.fourthGearRatio = 1.667;
+        this.fifthGearRatio = 1.285;
+        this.sixthGearRatio = 1.000;
+        this.seventhGearRatio = 0.839;
+        this.eightGearRatio = 0.667;
+    }
 }
