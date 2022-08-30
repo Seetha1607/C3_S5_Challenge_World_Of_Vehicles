22581c927aded3ddb0f79a871df34ffa2f899974
Seetha <seethalakshmi1607@gmail.com>
2022-08-31 00:44:13 +0530

    created getter and setter methods in AutomaticCVT.java class

--- src/main/java/com/vehicles/AutomaticCVT.java
+++ src/main/java/com/vehicles/AutomaticCVT.java
@@ -14,6 +14,62 @@ public class AutomaticCVT extends Transmission {
     private double fourthGearRatio;
     private double fifthGearRatio;
     private double sixthGearRatio;
-    private double seventhGearRatio;
-    private double eightGearRatio;
+
+    public AutomaticCVT(String transmissionType, String modelNumber, int forwardGears, double firstGearRatio, double secondGearRatio, double thirdGearRatio, double fourthGearRatio, double fifthGearRatio, double sixthGearRatio) {
+        super("Automatic-CVT", "CVT6", 6);
+        this.firstGearRatio = 2.631;
+        this.secondGearRatio = 1.440;
+        this.thirdGearRatio = 1.165;
+        this.fourthGearRatio = 0.906;
+        this.fifthGearRatio = 0.680;
+        this.sixthGearRatio = 0.499;
+    }
+
+    public double getFirstGearRatio() {
+        return firstGearRatio;
+    }
+
+    public void setFirstGearRatio(double firstGearRatio) {
+        this.firstGearRatio = firstGearRatio;
+    }
+
+    public double getSecondGearRatio() {
+        return secondGearRatio;
+    }
+
+    public void setSecondGearRatio(double secondGearRatio) {
+        this.secondGearRatio = secondGearRatio;
+    }
+
+    public double getThirdGearRatio() {
+        return thirdGearRatio;
+    }
+
+    public void setThirdGearRatio(double thirdGearRatio) {
+        this.thirdGearRatio = thirdGearRatio;
+    }
+
+    public double getFourthGearRatio() {
+        return fourthGearRatio;
+    }
+
+    public void setFourthGearRatio(double fourthGearRatio) {
+        this.fourthGearRatio = fourthGearRatio;
+    }
+
+    public double getFifthGearRatio() {
+        return fifthGearRatio;
+    }
+
+    public void setFifthGearRatio(double fifthGearRatio) {
+        this.fifthGearRatio = fifthGearRatio;
+    }
+
+    public double getSixthGearRatio() {
+        return sixthGearRatio;
+    }
+
+    public void setSixthGearRatio(double sixthGearRatio) {
+        this.sixthGearRatio = sixthGearRatio;
+    }
 }
