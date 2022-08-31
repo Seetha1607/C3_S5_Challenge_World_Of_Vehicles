f53f78a2e7345984f338ecb7af6757c76f1a160e
Seetha <seethalakshmi1607@gmail.com>
2022-08-31 11:55:19 +0530

    created showSpecs() method in Transmission class

--- src/main/java/com/vehicles/Transmission.java
+++ src/main/java/com/vehicles/Transmission.java
@@ -8,40 +8,25 @@
 package com.vehicles;
 
 public class Transmission {
+    private String transmissionType;
     private String modelNumber;
     private int forwardGears;
-    private float firstGearRatio;
-    private float secondGearRatio;
-    private float thirdGearRatio;
-    private float fourthGearRatio;
-    private float fifthGearRatio;
-    private float sixthGearRatio;
-    private float seventhGearRatio;
-    private float eightGearRatio;
 
     public Transmission() {
     }
 
-    public Transmission(String modelNumber,
-                        int forwardGears,
-                        float firstGearRatio,
-                        float secondGearRatio,
-                        float thirdGearRatio,
-                        float fourthGearRatio,
-                        float fifthGearRatio,
-                        float sixthGearRatio,
-                        float seventhGearRatio,
-                        float eightGearRatio) {
+    public Transmission(String transmissionType, String modelNumber, int forwardGears) {
+        this.transmissionType = transmissionType;
         this.modelNumber = modelNumber;
         this.forwardGears = forwardGears;
-        this.firstGearRatio = firstGearRatio;
-        this.secondGearRatio = secondGearRatio;
-        this.thirdGearRatio = thirdGearRatio;
-        this.fourthGearRatio = fourthGearRatio;
-        this.fifthGearRatio = fifthGearRatio;
-        this.sixthGearRatio = sixthGearRatio;
-        this.seventhGearRatio = seventhGearRatio;
-        this.eightGearRatio = eightGearRatio;
+    }
+
+    public String getTransmissionType() {
+        return transmissionType;
+    }
+
+    public void setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
     }
 
     public String getModelNumber() {
@@ -60,67 +45,6 @@ public class Transmission {
         this.forwardGears = forwardGears;
     }
 
-    public float getFirstGearRatio() {
-        return firstGearRatio;
-    }
-
-    public void setFirstGearRatio(float firstGearRatio) {
-        this.firstGearRatio = firstGearRatio;
-    }
-
-    public float getSecondGearRatio() {
-        return secondGearRatio;
-    }
-
-    public void setSecondGearRatio(float secondGearRatio) {
-        this.secondGearRatio = secondGearRatio;
-    }
-
-    public float getThirdGearRatio() {
-        return thirdGearRatio;
-    }
-
-    public void setThirdGearRatio(float thirdGearRatio) {
-        this.thirdGearRatio = thirdGearRatio;
-    }
-
-    public float getFourthGearRatio() {
-        return fourthGearRatio;
-    }
-
-    public void setFourthGearRatio(float fourthGearRatio) {
-        this.fourthGearRatio = fourthGearRatio;
-    }
-
-    public float getFifthGearRatio() {
-        return fifthGearRatio;
-    }
-
-    public void setFifthGearRatio(float fifthGearRatio) {
-        this.fifthGearRatio = fifthGearRatio;
-    }
-
-    public float getSixthGearRatio() {
-        return sixthGearRatio;
-    }
-
-    public void setSixthGearRatio(float sixthGearRatio) {
-        this.sixthGearRatio = sixthGearRatio;
-    }
-
-    public float getSeventhGearRatio() {
-        return seventhGearRatio;
-    }
-
-    public void setSeventhGearRatio(float seventhGearRatio) {
-        this.seventhGearRatio = seventhGearRatio;
-    }
-
-    public float getEightGearRatio() {
-        return eightGearRatio;
-    }
-
-    public void setEightGearRatio(float eightGearRatio) {
-        this.eightGearRatio = eightGearRatio;
+    public void showSpecs() {
     }
 }
